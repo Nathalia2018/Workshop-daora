5488847a3c07a07fa00017ec1f50b83856a55050
nathalia <nathalialopesufv@gmail.com>
2018-06-30 00:39:53 -0300

    meu primeiro commit

---
+++ Python.py
@@ -0,0 +1,21 @@
+# a = int(input("Digite um numero: "))
+# b = int(input("Digite outro numero: "))
+# c = a + b
+# print("A soma é", c)
+# c = a - b
+# print("A subtração é", c)
+# c = a * b
+# print("A multiplicação é", c)
+# c = a/b
+# print("A divisão é", c)
+# c = a ** b
+# print("A potência é", c)
+# x = float(input("Digite um numero: "))
+# print(x)
+# print("Ola"*3)
+print(len("respectivamente"))
+loteria = [1, 3, 4, 7]
+loteria.sort()
+print(loteria)
+loteria.reverse()
+print(loteria)
